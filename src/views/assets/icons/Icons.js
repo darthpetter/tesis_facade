48816665131bcde7fffb033cbef01dc9a9143a0d
petterjose <gavilanezseg@gmail.com>
2022-07-17 03:00:52 -0500

    interfaz: perfil y rrss

---
+++ src/views/assets/icons/Icons.js
@@ -0,0 +1,13 @@
+export const Icons={
+    facebook_icon,
+    instagram_icon,
+}
+
+
+function facebook_icon(){
+    return (<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M19,3H5C3.895,3,3,3.895,3,5v14c0,1.105,0.895,2,2,2h7.621v-6.961h-2.343v-2.725h2.343V9.309 c0-2.324,1.421-3.591,3.495-3.591c0.699-0.002,1.397,0.034,2.092,0.105v2.43h-1.428c-1.13,0-1.35,0.534-1.35,1.322v1.735h2.7 l-0.351,2.725h-2.365V21H19c1.105,0,2-0.895,2-2V5C21,3.895,20.105,3,19,3z"/></svg>)
+}
+
+function instagram_icon(){
+    return (<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">    <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"/></svg>)
+}
--- src/views/perfil_rrss/secciones/RRSS.js
+++ src/views/perfil_rrss/secciones/RRSS.js
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { Icons } from "../../assets/icons/Icons"
+
 
 export const RRSS = () => {
 
@@ -31,14 +33,43 @@ export const RRSS = () => {
             <div className="p-4 border border-gray-600 rounded-md grid grid-cols-2 gap-5">
                 <div className="mt-1 flex rounded-md shadow-sm">
                     <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                        http://
+                        {Icons.facebook_icon()}
                     </span>
                     <input
                         type="text"
-                        name="company-website"
-                        id="company-website"
+                        name="facebook_profile"
+                        id="facebook_profile"
+                        onChange={(e)=> handleRRSS(e)}
+                        value={rrss.facebook_profile}
                         className="p-2 focus:ring-guayaquil-500 focus:ring-1 focus:ring-opacity-40 focus:border-guayaquil-500 focus:outline-none block w-full shadow-sm border border-gray-400 rounded-md"
-                        placeholder="www.ejemplo.com"
+                        placeholder="@usuario"
+                    />
+                </div>
+                <div className="mt-1 flex rounded-md shadow-sm">
+                    <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                        {Icons.instagram_icon()}
+                    </span>
+                    <input
+                        type="text"
+                        name="instagram_profile"
+                        id="instagram_profile"
+                        onChange={(e)=> handleRRSS(e)}
+                        value={rrss.instagram_profile}
+                        className="p-2 focus:ring-guayaquil-500 focus:ring-1 focus:ring-opacity-40 focus:border-guayaquil-500 focus:outline-none block w-full shadow-sm border border-gray-400 rounded-md"
+                        placeholder="@usuario"
+                    />
+                </div>
+                <div className="mt-1 flex rounded-md shadow-sm">
+                    <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                    </span>
+                    <input
+                        type="text"
+                        name="twitter_profile"
+                        id="twitter_profile"
+                        onChange={(e)=> handleRRSS(e)}
+                        value={rrss.twitter_profile}
+                        className="p-2 focus:ring-guayaquil-500 focus:ring-1 focus:ring-opacity-40 focus:border-guayaquil-500 focus:outline-none block w-full shadow-sm border border-gray-400 rounded-md"
+                        placeholder="@usuario"
                     />
                 </div>
                 <div className="mt-1 flex rounded-md shadow-sm">
@@ -47,8 +78,10 @@ export const RRSS = () => {
                     </span>
                     <input
                         type="text"
-                        name="company-website"
-                        id="company-website"
+                        name="url_personal"
+                        id="url_personal"
+                        onChange={(e)=> handleRRSS(e)}
+                        value={rrss.url_personal}
                         className="p-2 focus:ring-guayaquil-500 focus:ring-1 focus:ring-opacity-40 focus:border-guayaquil-500 focus:outline-none block w-full shadow-sm border border-gray-400 rounded-md"
                         placeholder="www.ejemplo.com"
                     />
